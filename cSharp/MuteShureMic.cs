a479a6515f46336f6fad6c8e84c371fe84b63d28
Tomáš Okruhlica <tomasokruhlica@seznam.cz>
2024-12-28 20:24:31 +0100

    Fixed bug, looks for mic contains "Shure" keyword

--- cSharp/MuteShureMic.cs
+++ cSharp/MuteShureMic.cs
@@ -8,7 +8,7 @@ class MuteShureMic
     static void Main(string[] args)
     {
         // Enter your device name of the mic from USB connection
-        string inputUSBDeviceName = "Mikrofon (2 - Shure MV7+)";
+        string inputUSBDeviceName = "Mikrofon (Shure MV7+)";
         var enumerator = new MMDeviceEnumerator();
         var inputDevice = FindDeviceByName(enumerator, inputUSBDeviceName);
         // Make sure to setup your default device as your mic from the XLR connection
@@ -16,8 +16,13 @@ class MuteShureMic
 
         if (inputDevice == null)
         {
-            Console.WriteLine($"Microphone '{inputUSBDeviceName}' not found.");
-            return;
+            inputDevice = FindDeviceByName(enumerator, "Shure");
+
+            if (inputDevice == null)
+            {
+                Console.WriteLine("No microphone containing 'Shure' found.");
+                return;
+            }
         }
 
         while (true) // Loop indefinitely
@@ -28,7 +33,7 @@ class MuteShureMic
             // Mute/unmute the default input device based on the microphone's mute status
             defaultInputDevice.AudioEndpointVolume.Mute = isMuted;
 
-            Thread.Sleep(1000); // Sleep for 1 seconds before the next check
+            Thread.Sleep(200); // Sleep for 0.2 seconds before the next check
         }
     }
 
